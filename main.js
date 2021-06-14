c29f62828f55fd41af892031680d86bb149d5488
yinweisu <wyin@ucdavis.edu>
2021-06-14 16:45:48 -0700

    preprocess

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+
 const media_constraints = {
     video: {
         width: 1280,
         height: 720,
     }
 };
-
 const input_height = 224;
 const input_width = 224;
+
+const model_file = './gluoncv_exported_resnet18_v1.onnx';
+var session = undefined;
+const preprocesser = new Preprocessor();
 
 function handle_get_user_media_error(e) {
     switch(e.name) {
@@ -31,7 +35,7 @@ var processor = {
         var self = this;
         setTimeout(function () {
             self.timerCallback();
-      }, 100); // roughly 10 frames per second
+      }, 200); // roughly 5 frames per second
     },
 
     doLoad: function() {
@@ -47,40 +51,39 @@ var processor = {
         }, false);
     },
 
-    computeFrame: function() {
+    computeFrame: async function() {
         this.ctx1.drawImage(this.video, 0, 0, input_width, input_height);
         var frame = this.ctx1.getImageData(0, 0, input_width, input_height);
         var l = frame.data.length / 4;
         var rgb_frame = frame.data.slice(0, l*3);
         var rgb_frame_f32 = Float32Array.from(rgb_frame);
 
-        const r_mean = 0.485;
-        const g_mean = 0.456;
-        const b_mean = 0.406;
-        const r_std = 0.229;
-        const g_std = 0.224;
-        const b_std = 0.225;
+        preprocesser.normalize(rgb_frame_f32, l);
+        const image_tensor = new ort.Tensor('float32', rgb_frame_f32, [1,224,224,3]);
+        const result = await session.run({data: image_tensor});
+        const data = result.resnetv10_dense0_fwd.data;
+        console.log(data);
 
-        for (var i = 0; i < l; i++) {
-            var r = rgb_frame_f32[i * 3 + 0];
-            var g = rgb_frame_f32[i * 3 + 1];
-            var b = rgb_frame_f32[i * 3 + 2];
-            rgb_frame_f32[i * 3 + 0] = ((r / 255) - r_mean) / r_std;
-            rgb_frame_f32[i * 3 + 1] = ((g / 255) - g_mean) / g_std;
-            rgb_frame_f32[i * 3 + 2] = ((b / 255) - b_mean) / b_std;
-        }
-        console.log(rgb_frame_f32);
         // this.ctx1.putImageData(frame, 0, 0);
 
         return;
     }
 }; 
 
-navigator.mediaDevices.getUserMedia(media_constraints)
-.then(function (stream) {
-    var local_video_stream = document.getElementById('local_video_stream');
-    local_video_stream.srcObject = stream;
-    processor.doLoad();
-})
-.catch(handle_get_user_media_error);
+async function main() {
+    try {
+        session = await ort.InferenceSession.create(model_file);
+        navigator.mediaDevices.getUserMedia(media_constraints)
+        .then(function (stream) {
+            var local_video_stream = document.getElementById('local_video_stream');
+            local_video_stream.srcObject = stream;
+            processor.doLoad();
+        })
+        .catch(handle_get_user_media_error);
+    } catch (e) {
+        alert(e);
+    }
+    
+}
 
+main();
